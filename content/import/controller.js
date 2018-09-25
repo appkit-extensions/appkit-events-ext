850b885710841dd01fccd5e7fff50367261dc36c
Dave Weaver <dave@markkup.com>
2018-09-25 12:40:18 -0400

    find categories

--- content/import/controller.js
+++ content/import/controller.js
@@ -16,6 +16,16 @@ angular.module("moduleContent")
             var items = [];
             for (var i=0; i<events.length; i++) {
                 var event = new ICAL.Event(events[i]);
+
+                // find categories
+                var fields = event.component.jCal[1];
+                for (var j=0; j<fields.length; j++) {
+                    if (fields[i][0] === "categories") {
+                        event.categories = fields[i][0][3];
+                        break;
+                    }
+                }
+
                 items.push(event);
             }
             $scope.items = items;
