70fc8e001b56ee55a4a8d96664cd6f5f21fdfeda
Dave Weaver <dave@markkup.com>
2018-09-25 11:37:15 -0400

    show parsed events

--- content/import/controller.js
+++ content/import/controller.js
@@ -3,11 +3,22 @@ angular.module("moduleContent")
     function eventsImportContentCtrl($scope) {
 
         $scope.error = "#ERROR#";
+        $scope.hasThumb = false;
+
         $scope.textICal = "";
         $scope.resultsICal = "";
+        $scope.items = [];
 
         $scope.parseICal = function () {
-            $scope.resultsICal = JSON.stringify(ICAL.parse($scope.textICal));
+            var data = ICAL.parse($scope.textICal);
+            var calendar = new ICAL.Component(data);
+            var events = calendar.getFirstSubcomponent('vevent');
+            var items = [];
+            for (var i=0; i<events.length; i++) {
+                var event = new ICAL.Event(events[i]);
+                items.push(event);
+            }
+            $scope.items = items;
         }
     }
 ]);
