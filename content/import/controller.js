3b95af087e89aed5744938eda69ce9c52a02cb40
Dave Weaver <dave@markkup.com>
2018-09-25 11:24:55 -0400

    added ical import

--- content/import/controller.js
+++ content/import/controller.js
@@ -1,8 +1,15 @@
+<script src="https://rawgit.com/mozilla-comm/ical.js/master/build/ical.js" />
+
 angular.module("moduleContent")
 .controller("eventsImportContentCtrl", ["$scope", "$attrs", "$parse",
     function eventsImportContentCtrl($scope) {
 
         $scope.error = "#ERROR#";
-        $scope.name = "Dave";
+        $scope.textICal = "";
+        $scope.resultsICal = "";
+
+        $scope.parseICal = function () {
+            $scope.resultsICal = JSON.stringify(ICAL.parse($scope.textICal));
+        }
     }
 ]);
